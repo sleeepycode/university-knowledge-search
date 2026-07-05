4cd268a5817d4a9a13afb6a77bc4bd7ed25b83a6
GloryofRussia <ivankhalimon@gmail.com>
2026-07-06 01:23:56 +0300

    доработка locust

--- tests/locust_test/locustfile.py
+++ tests/locust_test/locustfile.py
@@ -1,11 +1,25 @@
+import random
 from locust import HttpUser, task, constant
 
 class WebsiteUser(HttpUser):
-    # Задаем фиксированное время ожидания (0 секунд) между запросами, 
-    # чтобы пользователи генерировали плотный поток нагрузки.
     wait_time = constant(0)
 
     @task
     def search_request(self):
-        # Отправляем GET-запрос на нужный эндпоинт
-        self.client.get("/api/v1/search")
+        # Массив тестовых запросов
+        queries = [
+            "документ",
+            "отчёт",
+            "инструкция",
+            "правила",
+            "политика"
+        ]
+        
+        self.client.get(
+            "/api/v1/search",
+            params={
+                "q": random.choice(queries),
+                "page": 1,
+                "page_size": 10
+            }
+        )
